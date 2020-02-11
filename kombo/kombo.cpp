9984af87798ac4f96b706b5c4c9cdd17f9719ca3
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-02-11 23:09:24 +0300

    Залил задачу №3

--- kombo/kombo.cpp
+++ kombo/kombo.cpp
@@ -1,23 +1,103 @@
 ﻿#include <iostream>
-#include <boost/timer.hpp>
-#include <vector>
 #include <algorithm>
 #include <iterator>
+#include <map>
+#include <ctime>
+#include <cassert>
 
 using namespace std;
 
+
+struct PRNG
+{
+    unsigned seed = 0;
+};
+
+class Deck
+{
+public:
+
+    Deck()
+    {
+        initGenerator();
+        initDeckOfCards();
+    }
+
+
+    map <int, int> getDeckOfCards()
+    {
+        return deckOfCards;
+    }
+
+    void randomize()
+    {
+        initRandNumberForEachCard();
+        for (int i = 1; i <= quantityOfCardsPerDeck; ++i)
+        {
+            int tmp = deckOfCards[i];
+            deckOfCards[i] = deckOfCards[randNumberForEachCard[i]];
+            deckOfCards[randNumberForEachCard[i]] = tmp;
+        }
+    }
+
+    void showDeckWithMessage(string message)
+    {
+        // copy(deckOfCards.begin(), deckOfCards.end(), ostream_iterator<int>(cout, " ")); don't work with std::map, need int*, but its makes more probmlems then solves
+        cout << message << endl;
+        for (int i = 1; i <= quantityOfCardsPerDeck; ++i)
+        {
+            cout << deckOfCards[i] << " ";
+        }
+        cout << endl << endl;
+    }
+
+private:
+    map <int, int> deckOfCards;
+    map <int, int> randNumberForEachCard;
+    int quantityOfCardsPerDeck = 36;
+    PRNG generator;
+
+
+    void initDeckOfCards()
+    {
+        for (int i = 1, j = quantityOfCardsPerDeck; i <= quantityOfCardsPerDeck; ++i, j--)
+        {
+            deckOfCards[i] = j;
+        }
+    }
+
+    void initRandNumberForEachCard()
+    {
+        for (int i = 1; i <= quantityOfCardsPerDeck; ++i)
+        {
+            randNumberForEachCard[i] = random(1, quantityOfCardsPerDeck);
+        }
+    }
+
+    void initGenerator()
+    {
+        generator.seed = unsigned(std::time(nullptr));
+    }
+
+    unsigned random( unsigned minValue, unsigned maxValue)
+    {
+        assert(minValue < maxValue);
+        generator.seed = (generator.seed * 73129 + 95121);
+        return (generator.seed % (maxValue + 1 - minValue)) + minValue;
+    }
+
+};
+
 int main()
 {
-    vector <size_t> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    boost::timer t;
-    t.restart();
-    sort(v.begin(), v.end());
-    do
-    {
-       
-    } while (next_permutation(v.begin(), v.end()));
-    double duration = t.elapsed();
-    cout << duration << endl;
+    Deck deck;
+
+    deck.showDeckWithMessage("normal deck of cards is:");
+    deck.randomize();
+    deck.showDeckWithMessage("randomize deck of cards is:");
+    deck.randomize();
+    deck.showDeckWithMessage("and again:");
+
     return 0;
 }
 
